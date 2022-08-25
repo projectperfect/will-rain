b6730820db770d98084e4a56b8f773c6a8cad1d3
projectperfect <jprfct@protonmail.com>
2022-08-25 07:56:03 -0500

    weather parameters

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ import requests
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
 api_key = ""
 
+weather_params = {
+    "lat": 36.162663,
+    "lon": -86.781601,
+    "appid": api_key,
+    "exclude": "current,minutely,daily",
+}
